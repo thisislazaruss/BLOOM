a4a67b2fd057a4d6f419f039fa10715f3bf21bbf
Srishti <srishtipoudel2385@gmail.com>
2022-08-06 16:23:37 +0545

    added index.css

--- deploy/Bloom/views.py
+++ deploy/Bloom/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import *
 import tensorflow as tf
+import h5py
 
 # from django.core.files.storage import FileSystemStorage
 
@@ -11,6 +12,7 @@ import tensorflow as tf
 
 
 def success(request):
+    
     loaded_model = tf.keras.models.load_model("template/prekshyaandsrishti.h5")
     loaded_model.build()
     return render(request, 'success.html')
@@ -28,3 +30,28 @@ def image_upload(request):
     else:
         form = ImageForm()
     return render(request, 'image_upload.html', {'form': form})
+
+
+# # Intermediate resizing as a bottleneck.
+#     bottleneck = layers.Resizing(
+#         *TARGET_SIZE, interpolation=interpolation
+#     )(x)
+
+#     # Residual passes.
+#     for _ in range(num_res_blocks):
+#         x = res_block(bottleneck)
+
+#     # Projection.
+#     x = layers.Conv2D(
+#         filters=filters, kernel_size=3, strides=1, padding="same", use_bias=False
+#     )(x)
+#     x = layers.BatchNormalization()(x)
+
+#     # Skip connection.
+#     x = layers.Add()([bottleneck, x])
+
+#     # Final resized image.
+#     x = layers.Conv2D(filters=3, kernel_size=7, strides=1, padding="same")(x)
+#     final_resize = layers.Add()([naive_resize, x])
+
+#     return tf.keras.Model(inputs, final_resize, name="learnable_resizer")
