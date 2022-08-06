099fcd4f2f0d3b320692864c187116623edf0803
Prekshya Dawadi <prekshya019@gmail.com>
2022-08-06 11:15:28 +0000

    Passing image to the model attempt 1

--- deploy/Bloom/views.py
+++ deploy/Bloom/views.py
@@ -1,9 +1,11 @@
 # Create your views here.
 from cgitb import text
+from email.mime import image
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import *
 import tensorflow as tf
+import numpy as np
 
 # from django.core.files.storage import FileSystemStorage
 
@@ -12,7 +14,14 @@ import tensorflow as tf
 
 def success(request):
     loaded_model = tf.keras.models.load_model("templates/prekshyaandsrishti.h5")
-    loaded_model.build(256)
+    #loaded_model.build(256)
+    img = "media/image.jpg"
+    img1 = np.array(img)
+    #loaded_model.predict16(img, loaded_model)
+    # np.testing.assert_allclose(
+    # loaded_model(img1)
+    # )
+    loaded_model.__call__(img1)
     return render(request, 'success.html')
 
 
--- deploy/deploy/settings.py
+++ deploy/deploy/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'deploy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'media'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
