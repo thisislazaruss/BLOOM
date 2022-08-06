aa422d001bc8c83d95ff29e9e800e6114eaf0cf9
Prekshya Dawadi <prekshya019@gmail.com>
2022-08-06 15:05:24 +0000

    Feeding images to model attempt 2

--- deploy/Bloom/views.py
+++ deploy/Bloom/views.py
@@ -1,28 +1,37 @@
 # Create your views here.
 from cgitb import text
+from dataclasses import dataclass
 from email.mime import image
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import *
 import tensorflow as tf
 import numpy as np
-
+from PIL import Image
+import cv2
+from deploy import settings
+import os
 # from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
+INPUT_SIZE = (256, 256)
 
 def success(request):
     loaded_model = tf.keras.models.load_model("templates/prekshyaandsrishti.h5")
-    #loaded_model.build(256)
-    img = "media/image.jpg"
-    img1 = np.array(img)
-    #loaded_model.predict16(img, loaded_model)
-    # np.testing.assert_allclose(
-    # loaded_model(img1)
-    # )
-    loaded_model.__call__(img1)
-    return render(request, 'success.html')
+    path = settings.MEDIA_ROOT
+    img_list = os.listdir(path)
+    context = {"images": img_list}
+    #print(context)
+    image = tf.image.resize(context, (256,256))
+    image_tensor = tf.convert_to_tensor(img_list, dtype=tf.float32)
+    i#mage_tensor = tf.expand_dims(image_tensor, 0)
+    #image = np.array(image)
+    #image= image.resize((64,64))
+    #image = tf.reshape(image, shape=(3, 256,256, 3))
+    
+    prediction = loaded_model.__call__(image)
+    return render(request, 'success.html', context)
 
 
 def image_upload(request):
